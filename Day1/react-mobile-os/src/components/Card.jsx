c9b24fd26b81199210b9950a1c5c3f66a25929e0
SayedMajid <sydmajid04@gmail.com>
2022-05-16 20:46:16 +0530

    finish

--- Day1/react-mobile-os/src/components/Card.jsx
+++ Day1/react-mobile-os/src/components/Card.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import styles from '../styles/Card.module.css'
 
 export const Card = () => {
     return(
         <>
-            <div>
+            <div className={styles.div1}>
                 <h1>Mobile Operating System</h1>
-                <ul>
+                <ul style={{lineHeight:'25px'}}>
                     <li>Android</li>
                     <li>Blackberry</li>
                     <li>iPhone</li>
                     <li>Windows Phone</li>
                 </ul>
             </div>
-            <div>
+            <div className={styles.div1}>
                 <h1>Mobile Manufacturers</h1>
-                <ul>
+                <ul style={{lineHeight:'25px'}}>
                     <li>Samsung</li>
                     <li>HTC</li>
                     <li>Micromax</li>
