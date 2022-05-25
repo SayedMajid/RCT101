98ef83e48fd5f5c658353051d576811f0ad5a38e
SayedMajid <sydmajid04@gmail.com>
2022-05-25 22:27:32 +0530

    timer

---
+++ Day7/timer/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, { useEffect } from "react"
+
+function App() {
+
+  const [seconds, setSeconds] = React.useState(0);
+  const [mins, setMins] = React.useState(0);
+
+  let timer;
+  let endPoint = 59
+  
+  useEffect(() => {
+    
+    timer = setInterval(() => {
+      setSeconds(seconds + 1)
+      if(seconds === endPoint){
+        setMins(mins + 1);
+        setSeconds(0)
+      }
+    },1000)
+
+    return () => clearInterval(timer)
+  })
+
+  const resetTimer = () => {
+    setSeconds(0);
+    setMins(0);
+  }
+
+  const stopTimer = () => {
+    clearInterval(timer)
+  }
+
+  return (
+    <div className="App">
+      <h1>My Timer App</h1>
+      <h1>{mins} : {seconds}</h1>
+      <button onClick={resetTimer}>Reset</button>
+      <button onClick={stopTimer}>Stop</button>
+    </div>
+  );
+}
+
+export default App;
