991501f49f5248cc4633a48f874700a96350b2b7
SayedMajid <sydmajid04@gmail.com>
2022-05-31 14:58:03 +0530

    Passing props is difficult in nested components

--- day10-context-api/src/App.js
+++ day10-context-api/src/App.js
@@ -1,28 +1,12 @@
-import './App.css';
 import React, {useReducer} from 'react'
-
-const reducer = (counter, action) => {
-  switch (action.type){
-    case 'Increment':{
-      return counter + 1
-    }
-    case 'Decrement':{
-      if(counter > 0) return counter - 1
-    }
-    default:{
-      console.log('Type Incorrect', action.type)
-      return counter
-    }
-  }
-}
+// import Counter from './Counter';
 
 function App() {
-  const [counter , dispatch] = useReducer(reducer , 0)
+  
   return (
     <div className="App">
-      <h1>Counter : {counter}</h1>
-      <button onClick={() => dispatch({type : 'Increment'})}> + </button>
-      <button onClick={() => dispatch({type : 'Decrement'})}> - </button>
+      {/* <Counter/> */}
+      
     </div>
   );
 }
