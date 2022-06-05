fd904103a097fe1c9012dabcd33a853316f2286b
SayedMajid <sydmajid04@gmail.com>
2022-06-05 23:58:17 +0530

    changes

--- day11-routing1/src/App.js
+++ day11-routing1/src/App.js
@@ -1,17 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Products from "./Components/Products";
+import Product from "./Components/Product";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Home/>
-      <About/>
-      <Products/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/products/:id" element={<Product/>}/>
+      </Routes>
     </div>
   );
 }
