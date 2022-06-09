aa72613272a6970ff53b472dc57541a0d2f6eb0c
SayedMajid <sydmajid04@gmail.com>
2022-06-09 23:58:02 +0530

    added functionality plus/minus/etc...

--- day13-redux-intro/src/App.js
+++ day13-redux-intro/src/App.js
@@ -1,32 +1,41 @@
-import { Box, Button, Spacer, Text } from "@chakra-ui/react";
+import { Box, Button, Input, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AddIcon, MinusIcon } from "@chakra-ui/icons";
 import "./App.css";
 import { counter_decrement, counter_increment } from "./Store/action.types";
+import { add, addValue, substract , subValue, multiplyValue, divide} from "./Store/action";
 function App() {
   const dispatch = useDispatch();
 
   const state = useSelector((state) => state.count);
 
-  console.log(state);
+  const [data, setData] = useState("")
 
   return (
-    <Box bg="teal" h="100vh">
+    <Box bg="rose" h="100vh">
       <Box w="fit-content" m="auto" pt="20px">
-        <Text fontSize="2xl" color="white">Counter : {state}</Text>
+        <Text fontSize="2xl" color="black">
+          Counter : {state}
+        </Text>
         <Box display="flex" justifyContent="space-between">
-          <Button
-            onClick={() => dispatch({ type: counter_increment })}
-            h="40px"
-            w="40px"
-          >
+          <Button onClick={() => dispatch(add())} h="40px" w="40px">
             <AddIcon />
           </Button>
-          <Button onClick={() => dispatch({ type: counter_decrement })}>
+          <Button onClick={() => dispatch(substract())}>
             <MinusIcon />
           </Button>
         </Box>
+      </Box>
+      {/* user input */}
+      <Box border="0px solid black" w="fit-content" m="auto">
+        <Input type="number" placeholder="Enter any number" value={data} onChange={(e) => setData(e.target.value)}/>
+      </Box>
+      <Box w="fit-content" m="auto">
+        <Button onClick={() => dispatch(addValue(data))}>Add</Button>
+        <Button onClick={() => dispatch(subValue(data))}>substract</Button>
+        <Button onClick={() => dispatch(multiplyValue(data))}>Multiply</Button>
+        <Button onClick={() => dispatch(divide(data))}>Divide</Button>
       </Box>
     </Box>
   );
--- day13-redux-intro/src/Store/reducer.js
+++ day13-redux-intro/src/Store/reducer.js
@@ -1,7 +1,9 @@
-import { counter_decrement, counter_increment } from "./action.types";
+import { counter_decrement, counter_increment, counter_addValue, counter_subValue, counter_multiplyValue, counter_divide} from "./action.types";
 
 export const reducer  = (state , action) => {
-    
+    let value = +action.payload;
+    console.log(value)
+
     switch(action.type){
         case counter_increment:{
             {state.count++}
@@ -9,6 +11,22 @@ export const reducer  = (state , action) => {
         }
         case counter_decrement:{
             state.count--;
+            return {...state}
+        }
+        case counter_addValue:{
+            state.count = state.count + value;
+            return {...state}
+        }
+        case counter_subValue:{
+            state.count = state.count - value;
+            return {...state}
+        }
+        case counter_multiplyValue:{
+            state.count = state.count * value;
+            return {...state}
+        }
+        case counter_divide:{
+            state.count = (state.count/value).toFixed(2)
             return {...state}
         }
         default:{
